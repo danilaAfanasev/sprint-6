f33bd68a6f792a82932515f9d381a6b3cb6da58e
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-02 15:37:10 +0300

    Добавить класс Task для задач

---
+++ sprint6/src/model/Task.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.util.Objects;
+
+public class Task {
+    protected String name;
+    protected String description;
+    protected int id;
+    protected Status status = Status.NEW;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Task(String name, String description, int id, Status status) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.status = status;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object object){
+        if (this == object){
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Task task = (Task) object;
+        return id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Задача{" +
+                "название='" + name + '\'' +
+                ", описание='" + description + '\'' +
+                ", id='" + id + '\'' +
+                ", статус='" + status + '}' + '\'';
+    }
+}
