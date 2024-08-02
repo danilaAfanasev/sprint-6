2f6dd6c7ba0845fb28257b57cb1fcbcde76c006e
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-02 15:40:57 +0300

    Добавить класс Main для просмотра истории

---
+++ sprint6/src/Main.java
@@ -0,0 +1,54 @@
+import controllers.*;
+import model.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TaskManager inMemoryTaskManager = Managers.getDefault();
+
+        Task task1 = new Task("Задача", "123");
+        Task task2 = new Task("Задача", "123");
+        inMemoryTaskManager.addTask(task1);
+        inMemoryTaskManager.addTask(task2);
+
+        Epic epic3 = new Epic("Эпик", "123");
+        Epic epic4 = new Epic("Эпик", "123");
+        inMemoryTaskManager.addEpic(epic3);
+        inMemoryTaskManager.addEpic(epic4);
+
+        Subtask subtask5 = new Subtask("Подзадача", "123", 3);
+        Subtask subtask6 = new Subtask("Подзадача", "123", 3);
+        Subtask subtask7 = new Subtask("Подзадача", "123", 3);
+        inMemoryTaskManager.addSubtask(subtask5);
+        inMemoryTaskManager.addSubtask(subtask6);
+        inMemoryTaskManager.addSubtask(subtask7);
+
+        inMemoryTaskManager.getTaskById(task2.getId());
+        inMemoryTaskManager.getTaskById(task1.getId());
+        inMemoryTaskManager.getEpicById(epic4.getId());
+        inMemoryTaskManager.getEpicById(epic3.getId());
+        inMemoryTaskManager.getSubtaskById(subtask7.getId());
+        inMemoryTaskManager.getEpicById(epic4.getId());
+        inMemoryTaskManager.getEpicById(epic3.getId());
+        inMemoryTaskManager.getSubtaskById(subtask7.getId());
+        inMemoryTaskManager.getTaskById(task1.getId());
+        inMemoryTaskManager.getTaskById(task2.getId());
+        inMemoryTaskManager.getSubtaskById(subtask5.getId());
+        inMemoryTaskManager.getSubtaskById(subtask6.getId());
+        inMemoryTaskManager.getSubtaskById(subtask6.getId());
+        inMemoryTaskManager.getSubtaskById(subtask5.getId());
+
+        System.out.println();
+        System.out.println(inMemoryTaskManager.getHistory());
+
+        inMemoryTaskManager.removeTaskById(task1.getId());
+
+        System.out.println();
+        System.out.println(inMemoryTaskManager.getHistory());
+
+        inMemoryTaskManager.removeEpicById(epic3.getId());
+
+        System.out.println();
+        System.out.println(inMemoryTaskManager.getHistory());
+    }
+}
