980fedb0c8754eb7505d6f09705cd7fa5711dbe2
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-12 12:41:22 +0300

    Добавить метод getDefaultFile

--- sprint6/src/controllers/Managers.java
+++ sprint6/src/controllers/Managers.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.io.File;
+
 public class Managers {
     private Managers() {
     }
@@ -10,5 +12,9 @@ public class Managers {
 
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
+    }
+
+    public static TaskManager getDefaultFile() {
+        return new FileBackedTasksManager(new File("C:\\Users\\monst\\Desktop\\яндекса джава\\спринт 6\\sprint-6\\sprint6\\resources\\file.csv"));
     }
 }
