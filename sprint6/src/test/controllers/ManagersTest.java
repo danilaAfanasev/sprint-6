cfe67252a42b5071f8c46e881229c63660869cdf
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-12 12:35:36 +0300

    Добавить аннотацию к тестам

--- sprint6/src/test/controllers/ManagersTest.java
+++ sprint6/src/test/controllers/ManagersTest.java
@@ -1,17 +1,20 @@
-package test;
+package test.controllers;
 
 import controllers.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagersTest {
     @Test
+    @DisplayName("Должен инициализировать InMemoryTaskManager по умолчанию")
     void getDefaultShouldInitializeInMemoryTaskManager() {
         TaskManager taskManager = Managers.getDefault();
         assertTrue(taskManager instanceof InMemoryTaskManager);
     }
 
     @Test
+    @DisplayName("Должен инициализировать InMemoryTaskManager по умолчанию")
     void getDefaultHistoryShouldInitializeInMemoryHistoryManager() {
         HistoryManager historyManager = Managers.getDefaultHistory();
         assertTrue(historyManager instanceof InMemoryHistoryManager);
