99a6941f395eb9a3468abd4a6aa09f7a044fa809
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-25 18:36:46 +0300

    Добавить конструктор и обработку времени

--- sprint6/src/model/Epic.java
+++ sprint6/src/model/Epic.java
@@ -2,10 +2,13 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Epic extends Task {
 
     private List<Integer> subtaskIds = new ArrayList<>();
+    private LocalDateTime endTime;
 
     public Epic(String name, String description) {
         super(name, description);
@@ -20,6 +23,12 @@ public class Epic extends Task {
         this.subtaskIds = subtaskIds;
     }
 
+    public Epic(String name, String description, int id, Status status, LocalDateTime startTime, long duration,
+                LocalDateTime endTime) {
+        super(name, description, id, status, startTime, duration);
+        this.endTime = endTime;
+    }
+
     public List<Integer> getSubtaskIds() {
         return subtaskIds;
     }
@@ -30,12 +39,50 @@ public class Epic extends Task {
     }
 
     @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    @Override
+    public String getEndTimeString() {
+        if (endTime == null) {
+            return "null";
+        }
+        return getEndTime().format(formatter);
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Epic epic = (Epic) o;
+        return Objects.equals(name, epic.name) &&
+                Objects.equals(description, epic.description) &&
+                (id == epic.id) &&
+                Objects.equals(status, epic.status) &&
+                Objects.equals(subtaskIds, epic.subtaskIds)&&
+                Objects.equals(startTime, epic.startTime) &&
+                (duration == epic.duration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id, status, subtaskIds, startTime, duration);
+    }
+
+    @Override
     public String toString() {
         return "Эпик{" +
-                "название='" + name + '\'' +
-                ", описание='" + description + '\'' +
-                ", id='" + id + '\'' +
-                ", статус='" + status + '\'' +
-                ", id подзадач(и)='" + subtaskIds + '}' + '\'';
+                    "название='" + name + '\'' +
+                    ", описание='" + description + '\'' +
+                    ", id='" + id + '\'' +
+                    ", статус='" + status + '\'' +
+                    ", дата начала='" + getStartTimeString() + '\'' +
+                    ", продолжительность='" + duration + '\'' +
+                    ", id подзадач(и)='" + subtaskIds + '}' + '\'';
+        }
     }
-}
