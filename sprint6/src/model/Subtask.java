fad7f3c0cf145c48a5064e98670e10f185bdba51
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-25 18:37:12 +0300

    Добавить конструктор и обработку времени

--- sprint6/src/model/Subtask.java
+++ sprint6/src/model/Subtask.java
@@ -1,7 +1,10 @@
 package model;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Subtask extends Task {
-    private int epicId;
+    private final int epicId;
 
     public Subtask(String name, String description, int epicId) {
         super(name, description);
@@ -10,6 +13,12 @@ public class Subtask extends Task {
 
     public Subtask(String name, String description, int id, Status status, int epicId) {
         super(name, description, id, status);
+        this.epicId = epicId;
+    }
+
+    public Subtask(String name, String description, int id, Status status, int epicId, LocalDateTime startTime,
+                   long duration) {
+        super(name, description, id, status, startTime, duration);
         this.epicId = epicId;
     }
 
@@ -23,12 +32,33 @@ public class Subtask extends Task {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subtask subtask = (Subtask) o;
+        return Objects.equals(name, subtask.name) &&
+                Objects.equals(description, subtask.description) &&
+                (id == subtask.id) &&
+                Objects.equals(status, subtask.status) &&
+                (epicId == subtask.epicId) &&
+                Objects.equals(startTime, subtask.startTime) &&
+                (duration == subtask.duration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, id, status, startTime, duration, epicId);
+    }
+
+    @Override
     public String toString() {
         return "Подзадача{" +
                 "название='" + name + '\'' +
                 ", описание='" + description + '\'' +
                 ", id='" + id + '\'' +
                 ", статус='" + status + '\'' +
+                ", дата начала='" + getStartTimeString() + '\'' +
+                ", продолжительность='" + duration + '\'' +
                 ", id эпика='" + epicId + '}' + '\'';
     }
 }
