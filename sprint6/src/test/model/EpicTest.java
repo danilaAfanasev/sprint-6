2a1fefa0eccff0e35d0a76a320046033e7e4a68d
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-08-26 12:02:11 +0300

    Отредактировать тест эпиков

--- sprint6/src/test/model/EpicTest.java
+++ sprint6/src/test/model/EpicTest.java
@@ -13,7 +13,7 @@ class EpicTest {
     @Test
     @DisplayName("Эпики с одинаковыми id должны быть идентичными")
     public void EpicsWithEqualIdShouldBeEqual() {
-        Epic epic1 = new Epic("Epic 1", "Описание 1", 1, Status.NEW, Arrays.asList(1, 2, 3));
+        Epic epic1 = new Epic("Epic 1", "Описание 1", 1,  Status.NEW, Arrays.asList(1, 2, 3));
         Epic epic2 = new Epic("Epic 2", "Описание 2", 1, Status.NEW, Arrays.asList(4, 5, 6));
         assertEquals(epic1, epic2, "Эпики с одинаковым id должны быть равны");
     }
