d3096b809b134321f671e6cf717ee483397f34c4
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-09-06 17:25:22 +0300

    Добавить обработчик для Subtask

---
+++ sprint6/src/http/SubtaskHandler.java
@@ -0,0 +1,107 @@
+package http;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import controllers.TaskManager;
+import model.Subtask;
+import exceptions.CollisionTaskException;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class SubtaskHandler extends BaseHttpHandler implements HttpHandler {
+
+    public SubtaskHandler(TaskManager taskManager) {
+        super(taskManager);
+    }
+
+    @Override
+    public void handle(HttpExchange ex) {
+        String method = ex.getRequestMethod();
+        String[] split = ex.getRequestURI().getPath().split("/");
+        try {
+            switch (method) {
+                case "GET":
+                    if (split.length == 2) {
+                        handleGetSubtasksResponse(ex);
+                    } else {
+                        if (getId(ex).isPresent()) {
+                            handleGetSubtaskByIdResponse(ex, getId(ex).get());
+                        } else {
+                            sendIncorrectId(ex);
+                        }
+                    }
+                    break;
+                case "DELETE":
+                    if (getId(ex).isPresent()) {
+                        handleDeleteSubtask(ex, getId(ex).get());
+                    } else {
+                        sendIncorrectId(ex);
+                    }
+                    break;
+                case "POST":
+                    handlePostSubtask(ex);
+                    break;
+                default:
+                    sendIncorrectMethod(ex);
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Во время выполнения запроса произошла ошибка. Проверьте URL");
+        }
+    }
+
+    public void handleGetSubtasksResponse(HttpExchange ex) {
+        try {
+            if (taskManager.getListOfSubtasks().isEmpty()) {
+                sendText(ex, "Список задач пуст", 404);
+            } else {
+                sendText(ex, gson.toJson(taskManager.getListOfSubtasks()), 200);
+            }
+        } catch (IOException e) {
+            System.out.println("Во время выполнения запроса произошла ошибка. Проверьте URL");
+        }
+    }
+
+    public void handleGetSubtaskByIdResponse(HttpExchange ex, int id) throws IOException {
+        if (!taskManager.getListOfSubtasks().contains(id)) {
+            sendText(ex, "Задача не найдена", 404);
+        } else {
+            sendText(ex, gson.toJson(taskManager.getSubtaskById(id)), 200);
+        }
+    }
+
+    public void handleDeleteSubtask(HttpExchange ex, int id) throws IOException {
+        if (!taskManager.getListOfSubtasks().contains(id)) {
+            sendText(ex, "Задача не найдена", 404);
+        } else {
+            taskManager.removeSubtaskById(id);
+            sendText(ex, "Подзадача с id " + id + " удалена", 200);
+        }
+    }
+
+    public void handlePostSubtask(HttpExchange ex) throws IOException {
+        try {
+            String exchange = bodyToString(ex);
+            if (exchange.isEmpty()) {
+                sendText(ex, "Ничего не передано", 400);
+            } else {
+                Subtask subtask = gson.fromJson(exchange, Subtask.class);
+                Optional<Integer> id = Optional.of(subtask.getId());
+                taskManager.validate(subtask);
+
+                if (id.get() == 0) {
+                    taskManager.addSubtask(subtask);
+                    sendText(ex, "Подзадача добавлена", 201);
+                } else {
+                    taskManager.updateSubtask(subtask);
+                    sendText(ex, "Подзадача с id " + id.get() + " обновлена", 201);
+                }
+            }
+        } catch (CollisionTaskException e) {
+            sendText(ex, "Подзадача не добавлена, так как имеет наложение по времени", 406);
+        } catch (IOException e) {
+            System.out.println("Во время выполнения запроса произошла ошибка. Проверьте URL");
+        }
+    }
+}
